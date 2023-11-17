f394b39dcfb1eda08732553cfb1c9f6f8b47faea
DeadMakar <ssapientiaa@gmail.com>
2023-11-17 16:05:18 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,55 @@
-import { Component } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const onLeaveFeedback = name => {
+    setFeedback(prevState => {
+      return {
+        ...prevState,
+        [name]: prevState[name] + 1,
+      };
+    });
   };
 
-  countTotalFeedbacks() {
-    const { good, neutral, bad } = this.state;
-    const total = Object.values({ good, neutral, bad }).reduce(
-      (acc, value) => acc + value,
-      0
-    );
-    return total;
-  }
-
-  countPositiveFeedbackPercentage() {
-    const { good } = this.state;
-    const total = this.countTotalFeedbacks();
-    return total ? ((good / total) * 100).toFixed(2) : 0;
-  }
-
-  onLeaveFeedback = ({ target: { name } }) => {
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }));
+  const countTotalFeedbacks = () => {
+    return Object.values(feedback).reduce((acc, value) => acc + value, 0);
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedbacks();
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedbacks();
+    return total ? ((feedback.good / total) * 100).toFixed(2) : 0;
+  };
 
-    return (
-      <>
-        <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
-          onLeaveFeedback={this.onLeaveFeedback}
+  const total = countTotalFeedbacks();
+  const { good, neutral, bad } = feedback;
+
+  return (
+    <>
+      <FeedbackOptions
+        options={['good', 'neutral', 'bad']}
+        onLeaveFeedback={onLeaveFeedback}
+      />
+      {total === 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positivePercentage={countPositiveFeedbackPercentage()}
         />
-        {total === 0 ? (
-          <Notification message="There is no feedback" />
-        ) : (
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-        )}
-        <GlobalStyle />
-      </>
-    );
-  }
-}
+      )}
+      <GlobalStyle />
+    </>
+  );
+};
